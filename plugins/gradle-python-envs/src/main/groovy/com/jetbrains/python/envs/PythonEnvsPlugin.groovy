0caaece54fd09722f9f70c7cd8fa947205df0adb
akliuxingyuan <akliuxingyuan@gmail.com>
2026-09-05 14:31:41 +0800

    update plugin gradle-python-envs

--- plugins/gradle-python-envs/src/main/groovy/com/jetbrains/python/envs/PythonEnvsPlugin.groovy
+++ plugins/gradle-python-envs/src/main/groovy/com/jetbrains/python/envs/PythonEnvsPlugin.groovy
@@ -126,7 +126,7 @@ class PythonEnvsPlugin implements Plugin<Project> {
                             if (isWindows) {
                                 commandLine installer, "/InstallationType=JustMe", "/AddToPath=0", "/RegisterPython=0", "/S", "/D=${env.envDir}"
                             } else {
-                                commandLine "bash", installer, "-b", "-p", env.envDir
+                                commandLine "bash", installer, "-b", "-u", "-p", env.envDir
                             }
                         }
 
